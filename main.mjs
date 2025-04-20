5b8208b501f977a935747b4eb6dfd9ef707a104f
Antoine Cailly <ylliac14@gmail.com>
2025-04-20 14:13:47 +0200

    feat: :sparkles: make external link open in a new tab

--- main.mjs
+++ main.mjs
@@ -1,5 +1,6 @@
 import { Eleventy } from "@11ty/eleventy";
 import config from "./config.json" with { type: "json" };
+import mila from "markdown-it-link-attributes";
 
 const inputDirectory = config.source;
 const __dirname = import.meta.dirname;
@@ -12,11 +13,26 @@ await generateWebsite(inputDirectory, distDirectory);
 async function generateWebsite(inputDirectory, distDirectory) {
   const eleventy = new Eleventy(inputDirectory, distDirectory, {
     config: function (eleventyConfig) {
+      // Use markdown
       eleventyConfig.setTemplateFormats("md");
       
+      // Copy assets
       eleventyConfig.addPassthroughCopy(inputDirectory, {
         filter: ["**/*.png", "**/*.pdf", "**/*.mp4"],
       })
+
+      // Make the external links open in a new tab
+      // see https://dannywhite.net/notes/new-tab-links/
+      const milaOptions = {
+        matcher(href) {
+          return href.match(/^https?:\/\//);
+        },
+        attrs: {
+          target: "_blank",
+          rel: "noopener",
+        },
+      };
+      eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(mila, milaOptions));
     },
   });
   await eleventy.write();
